37450b1607438b26eeec3759d92e2c5b13629bed
raininfotech07-kartik <86714020+raininfotech07-kartik@users.noreply.github.com>
2023-02-27 17:17:22 +0530

    update status

--- codeigniter/application/controllers/Welcome.php
+++ codeigniter/application/controllers/Welcome.php
@@ -49,6 +49,19 @@ class Welcome extends CI_Controller {
 
 	public function callBack()
 	{
+		// You will get these fields on each callback
+		// status 
+		// confirmed
+		// verify_hash
+		// orderid
+		// userid
+		// amount
+		// received_amount
+		// addr
+		// txHash
+		// pay_date
+		// requestdate
+
 		$resStatus=false;  									//status for check your response is valid or not
 		$key='';
 		if($_POST['status']){ 								//it's identify that status and other data set or not
@@ -61,7 +74,13 @@ class Welcome extends CI_Controller {
 				$key = md5(($public_key."".$private_key."".$_POST['orderid'])); // generate key using md5 mehod with combination of public key, private key and order id
 				$resStatus = true; 							//set status as true for pass in response
 
-				// Here you can take action on success payment.
+				if($_POST['status']=="payment_confirm" && $_POST['confirmed']==0){
+					// payment detected but unconfirm transaction
+				}else if($_POST['status']=="payment_confirm" && $_POST['confirmed']==1){
+					// payment verified successfully
+				}else if($_POST['status']=="payment_unrecognized"){
+					// payment detected but amount not matched
+				}
 			}
 		}
 		// send key and status in response to payment gateway for verification
--- mern/node-js/app.js
+++ mern/node-js/app.js
@@ -29,21 +29,40 @@ app.post('/get-hash', (req, res) => {
 })
 
 app.post('/callback', (req, res) => {
+  	// You will get these fields on each callback
+		// status 
+		// confirmed
+		// verify_hash
+		// orderid
+		// userid
+		// amount
+		// received_amount
+		// addr
+		// txHash
+		// pay_date
+		// requestdate
+    
     try{
         let resStatus=false;                                    //status for check your response is valid or not
         let key='';
-        const {status, orderid, userid}=req.body
+        const {status, confirmed, verify_hash, orderid, userid, amount, received_amount, addr, txHash, pay_date, requestdate}=req.body
         if(req.body.status){                                    //it's identify that status and other data set or not
     
             let isExist="FIRE_YOUR_DATABASE_QUERY_HERE";        // get record from database where DB_OrderId==orderid and DB_UserId==userid
             if (isExist) {
-              let public_key="GM****trxN"; 				        // Public Key
-              let private_key="7l*****PWs"; 			        // Private Key
+              let public_key="GM****trxN"; 				              // Public Key
+              let private_key="7l*****PWs"; 			              // Private Key
     
               key = md5(CONCAT(public_key,private_key,orderid));// generate key using md5 mehod with combination of public key, private key and order id
               resStatus = true;                                 //set status as true for pass in response
     
-              // Here you can take action on success payment.
+              if(status=="payment_confirm" && confirmed==0){
+                // payment detected but unconfirm transaction
+              }else if(status=="payment_confirm" && confirmed==1){
+                // payment verified successfully
+              }else if(status=="payment_unrecognized"){
+                // payment detected but amount not matched
+              }
             }
         }
         // send key and status in response to payment gateway for verification
